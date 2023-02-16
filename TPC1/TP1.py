ae1c4f081e54766e43368772b5f55265def4d1c1
Joel Barros <a94692@alunos.uminho.pt>
2023-02-16 00:15:45 +0000

    Update TP1.py

--- TPC1/TP1.py
+++ TPC1/TP1.py
@@ -21,18 +21,17 @@ def calculate_disease_by_sex():
     return res
 
 
-def find_min_max_age():
+def find_ages():
     idades = []
     data = parse()
     for linha in data:
-        idades.append(linha[0])
-    return idades
+        idades.append(int(linha[0]))
+    return min(idades), max(idades)
 
 
 def calculate_disease_by_age_groups():
     res = dict()
-    minimum = int(min(find_min_max_age()))
-    maximum = int(max(find_min_max_age()))
+    minimum, maximum = find_ages()
     while minimum < maximum:
         res.update({'[' + str(minimum) + '-' + str(minimum + 4) + ']': 0})
         minimum += 5
@@ -44,14 +43,44 @@ def calculate_disease_by_age_groups():
             if int(minimo) <= int(linha[0]) <= int(maximo):
                 res[key] += 1
                 break
+    return res
 
-    # print(sum(res.values()))
+
+def find_cholesterol_values():
+    values = []
+    data = parse()
+    for linha in data:
+        values.append(int(linha[3]))
+    return sorted(values), min(values), max(values)
+
+
+def calculate_by_cholesterol_levels():
+    res = dict()
+    level = 1
+    ordered_values, minimum, maximum = find_cholesterol_values()
+
+    temp = minimum
+
+    while temp < maximum:
+        res.update({level: 0})
+        level += 1
+        temp += 10
+
+    level = 1
+    for value in ordered_values:
+        while value > minimum + 9:
+            level += 1
+            minimum += 10
+        res[level] += 1
+        # print('[' + str(minimum) + '-' + str(minimum + 9) + ']' + ':' + str(res[level]))
     return res
 
 
 def main():
     # print(calculate_disease_by_sex())
-    print(calculate_disease_by_age_groups())
+    # print(calculate_disease_by_age_groups())
+    print(calculate_by_cholesterol_levels())
+    # print(find_cholesterol_values())
 
 
 if __name__ == '__main__':
